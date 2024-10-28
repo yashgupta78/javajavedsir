0321defed9bd97d426de430dc7b6d6fa16c0051d
anjali <anjaliaher08@gmail.com>
2024-10-28 07:50:27 +0530

    first

---
+++ basicprogramming/p8.java
@@ -0,0 +1,12 @@
+//wap to find the sum of all the number in the range 1 to 5 
+
+public class p8 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 5; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
